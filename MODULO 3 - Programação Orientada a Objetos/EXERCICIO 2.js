6db0f57ef8d6d4c7477042f4cb20d1c47bb894ea
João Lucas Cruz <63518059+jaotuta@users.noreply.github.com>
2021-10-19 22:14:52 +0000

    Entrega

--- MODULO 3 - Programação Orientada a Objetos/EXERCICIO 2.js
+++ MODULO 3 - Programação Orientada a Objetos/EXERCICIO 2.js
@@ -4,59 +4,58 @@ Grupo: Guilherme Lima, João Lucas, Rebeca Rodrigues, Talis Reis
 */
 
 class BookList {
-    
-    constructor() {
-      this.readBook = 0; 
-      this.notReadBook = 0;
-      this.nextBook = {};
-      this.currentBook = {};
-      this.lastBook = {};
-      this.allBooks = [];
-    }
+  constructor() {
+    this.readBook = 0;
+    this.notReadBook = 0;
+    this.nextBook = {};
+    this.currentBook = {};
+    this.lastBook = {};
+    this.allBooks = [];
+  }
 
-    add (newBook) {
+  add(newBook) {
+    this.allBooks = [...this.allBooks, newBook];
+    this.currentBook = this.allBooks.find((current) => current.read === false);
+    this._attNotReadBooks();
+  }
 
-        this.allBooks = [...this.allBooks, newBook];
-        this.currentBook = this.allBooks.find(current => current.read === false)
-        this.attNotReadBooks ()
-    }
-    attNotReadBooks () {
-      this.notReadBook = this.allBooks.reduce((acc, curr) => {(!curr.read) ? acc += 1 : acc 
-      return acc},0)
-    }
-    
-    finishCurrentBook() {
-        this.currentBook.read = true
-        this.currentBook.readDate = Date.now()
-        let proximo = this.allBooks.filter(current => !current.read)
-        this.nextBook = proximo[1]
-        this.lastBook = this.currentBook
-        this.currentBook = proximo[0]
-        this.readBook = this.allBooks.reduce((acc, curr) => {(curr.read) ? acc += 1 : acc 
-        return acc},0)
-        this.attNotReadBooks ()
-    }
+  _attNotReadBooks() {
+    this.notReadBook = this.allBooks.reduce((acc, curr) => {
+      !curr.read ? (acc += 1) : acc;
+      return acc;
+    }, 0);
+  }
 
-    
-
-
+  finishCurrentBook() {
+    if (this.currentBook) {
+      this.currentBook.read = true;
+      this.currentBook.readDate = new Date(Date.now());
+      let next = this.allBooks.filter((current) => !current.read);
+      this.nextBook = next[1];
+      this.lastBook = this.currentBook;
+      this.currentBook = next[0];
+      this.readBook = this.allBooks.reduce((acc, curr) => {
+        curr.read ? (acc += 1) : acc;
+        return acc;
+      }, 0);
+      this._attNotReadBooks();
+    }else{ return "book list is over"};
+  }
 }
 
 class Book {
-
-    constructor(title, genre, author, read, readDate) {
-      this.title = title; 
-      this.genre = genre;
-      this.author = author;
-      this.read = read;
-      this.readDate = (this.read) ? Date.now() : '';
-    }
+  constructor(title, genre, author, read, readDate) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.read = read;
+    this.readDate = this.read ? new Date(Date.now()) : "";
+  }
 }
 
-
-const book1 = new Book('Harry Potter', 'Aventura', 'J.K Rollbium', false);
-const book2 = new Book('Dom Quixote, Miguel de Cervantes', 'Ação', 'Marcel Proust', false);
-const book3 = new Book('Guerra e Paz, Liev Tolstói', 'Aventura', 'Liev Tolstói', false);
+const book1 = new Book('Harry Potter', 'Aventura', 'J.K Rowling', false);
+const book2 = new Book('Dom Quixote', 'Ação', 'Miguel de Cervantes', false);
+const book3 = new Book('Guerra e Paz', 'Aventura', 'Liev Tolstói', false);
 const book4 = new Book('A Montanha Mágica', 'Romance', 'Thomas Mann', false);
 const book5 = new Book('Cem Anos de Solidão', 'Terror', 'Gabriel García Márquez', false);
 const book6 = new Book('Ulisses', 'Aventura', 'James', false);
